a7786a7b1633e6e10887ba3281000bf888972396
Abhishek kesarwani <abhishekkesarwani269@gmail.com>
2025-09-18 01:20:35 +0530

    Time: 6 ms (88.85%) | Memory: 42.5 MB (53.46%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.length()!=t.length())
+       /* if(s.length()!=t.length())
         {
             return false;
 
@@ -36,6 +36,24 @@ class Solution {
          }
          
          return true;
-        
+         */
+         int map1[] = new int[256];
+         int map2[]= new int[256];
+         for(int i =0;i<s.length();i++)
+         {
+            char c1= s.charAt(i);
+            char c2=t.charAt(i);
+            if(map1[c1]!=0 && map1[c1]!=c2 )
+            {
+                return false;
+            }
+            if(map2[c2]!=0 && map2[c2]!=c1)
+            {
+                return false;
+            }
+            map1[c1]=c2;
+            map2[c2]=c1;
+         }
+        return true;
     }
 }

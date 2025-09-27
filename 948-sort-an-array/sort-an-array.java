4444fab4d2ff0083831df9cd088ebcf95da88ce1
Abhishek kesarwani <abhishekkesarwani269@gmail.com>
2025-09-27 19:48:37 +0530

    Time: 1 ms (100.00%) | Memory: 56.6 MB (36.64%) - LeetSync

--- 948-sort-an-array/sort-an-array.java
+++ 948-sort-an-array/sort-an-array.java
@@ -1,48 +1,13 @@
 class Solution {
-    public static void quicksort(int nums[] , int low , int high)
-    {
-        if(low < high)
-        {
-            int pivot = partition(nums , low , high);
-            quicksort(nums , low , pivot - 1);
-            quicksort(nums , pivot + 1 , high);
-        }
-    }
-
-    public static int partition(int nums[] , int low , int high)
-    {
-        // \U0001f511 Random pivot
-        int randIndex = low + (int)(Math.random() * (high - low + 1));
-        int tempSwap = nums[low];
-        nums[low] = nums[randIndex];
-        nums[randIndex] = tempSwap;
-
-        int x = nums[low];
-        int i = low;
-        int j = high;
-
-        while(i < j)
-        {
-            while(i <= high && nums[i] <= x)
-                i++;
-            while(j >= low && nums[j] > x)
-                j--;
-            if(i < j)
-            {
-                int temp = nums[i];
-                nums[i] = nums[j];
-                nums[j] = temp;
-            }
-        }
-        int temp = nums[low];
-        nums[low] = nums[j];
-        nums[j] = temp;
-
-        return j;
-    }
-
     public int[] sortArray(int[] nums) {
-        quicksort(nums , 0, nums.length - 1);
+    Thread sortThread = new Thread(() -> Arrays.sort(nums));//Dual-Pivot QuickSort
+        sortThread.start();
+        try{
+            sortThread.join();
+
+        }
+        catch(Exception e){
+        }
         return nums;
     }
 }

efcedce6b53a365174dedcd1166df2556d3764d2
Abhishek kesarwani <abhishekkesarwani269@gmail.com>
2025-10-06 20:29:03 +0530

    Time: 1 ms (99.71%) | Memory: 42.5 MB (27.24%) - LeetSync

--- 67-add-binary/add-binary.java
+++ 67-add-binary/add-binary.java
@@ -1,47 +1,29 @@
-import java.math.BigInteger;
-
 class Solution {
-
-    // Convert decimal (BigInteger) to binary string
-    public static String convertToBinary(BigInteger decimal) {
-        if (decimal.equals(BigInteger.ZERO)) {
-            return "0";
-        }
-        StringBuilder binary = new StringBuilder();
-        while (decimal.compareTo(BigInteger.ZERO) > 0) {
-            binary.append(decimal.mod(BigInteger.TWO));   // remainder 0 or 1
-            decimal = decimal.divide(BigInteger.TWO);     // divide by 2
-        }
-        return binary.reverse().toString();
-    }
-
     public String addBinary(String a, String b) {
-        // Convert binary strings to decimal using BigInteger
-        BigInteger dec1 = BigInteger.ZERO;
-        BigInteger dec2 = BigInteger.ZERO;
-        BigInteger pow2 = BigInteger.ONE; // starts with 2^0
-
-        // convert binary 'a' to decimal (from right to left)
-        for (int i = a.length() - 1; i >= 0; i--) {
-            if (a.charAt(i) == '1') {
-                dec1 = dec1.add(pow2);
-            }
-            pow2 = pow2.multiply(BigInteger.TWO);
+        if(a.length()<b.length()) {
+            return addBinary(b,a);
         }
+        int i = a.length()-1;
+        int j = b.length()-1;
+        int sum = 0;
+        int carry = 0;
+        char[] res = new char[i+2];
+        int k = i+1;
 
-        // reset pow2 and convert 'b'
-        pow2 = BigInteger.ONE;
-        for (int i = b.length() - 1; i >= 0; i--) {
-            if (b.charAt(i) == '1') {
-                dec2 = dec2.add(pow2);
+        while(i>=0) {
+            sum = carry+(a.charAt(i--)-'0');
+            if(j>=0) {
+                sum = sum+(b.charAt(j--)-'0');
             }
-            pow2 = pow2.multiply(BigInteger.TWO);
+            res[k--] = (char)(sum%2+'0');
+            carry = sum/2;
         }
-
-        // Add both decimal numbers
-        BigInteger sum = dec1.add(dec2);
-
-        // Convert back to binary
-        return convertToBinary(sum);
+        if(carry == 1) {
+            res[k]=(char)(carry+'0');
+        }
+        else if(res[k]!=1) {
+            return new String(res, 1, res.length-1);
+        }
+        return new String(res);
     }
 }

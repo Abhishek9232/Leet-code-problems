6509bd5e3e38a798eadf43c3501e58dd131b839a
Abhishek kesarwani <abhishekkesarwani269@gmail.com>
2025-11-07 14:32:02 +0530

    Time: 17 ms (29.92%) | Memory: 47.4 MB (5.16%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -7,18 +7,18 @@ class Solution {
             map.put(ch,map.getOrDefault(ch,0)+1);
         }
 
-         PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> a.getValue() - b.getValue());
+         PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
          pq.addAll(map.entrySet());
-        String res="";
+        StringBuilder res = new StringBuilder();
           while (!pq.isEmpty()) {
             Map.Entry<Character, Integer> entry = pq.poll();
             for(int i=0;i<entry.getValue();i++)
             {
-                res=entry.getKey()+res;
+                res.append(entry.getKey());
             }
             
         }
 
-        return res;
+        return res.toString();
     }
 }
